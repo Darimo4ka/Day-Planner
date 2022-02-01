76c46aa78c41030b1fefc97ba2ec0fa231def07f
Darimo4ka <darima_badm@yahoo.com>
2022-02-01 10:51:12 -0500

    some of the work in javascript but it is not fully working

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 //  ADD CURRENT DATE AT THE TOP OF THE PAGE
-var today = moment();
-$("#time-display").text(today.format("dddd, MMMM Do"));
+// var today = moment();
+// $("#time-display").text(today.format("dddd, MMMM Do"));
+
+
+// assign var to HTML
+var rowEl = $(".row"); 
+var saveBtn =$(".col-md-1 col-sm-2 saveBtn");
+var todayEl =$("#currentDay");
+var timeDisplay =$("#time-display");
+
+function getLocalstorage(){
+    rowEl.each(function() {
+        var saveTask = $(this).attr("id");
+        if(localStorage.getItem(saveTask) !==null){
+            $(this).children("input").val(localStorage.getItem(saveTask));
+        }
+            
+    });
+}
+// handle displaying the time
+function checkTime()  {
+
+    var now = moment().hour();
+
+    rowEl.each(function(){
+        var workTime = parseInt($(this).attr("id").split("row")[1]);
+    if (workTime< now){
+        $(this).children("input").addclass("past");
+    } else if (workTime === now){
+        $(this).children("input").addclass("present");}
+        else{
+            $(this).children("input").addclass("future");
+        }
+    });
+   }
+   $(document).ready(function(){
+    todayEl.text(moment().format("dddd,MMMM Do"));
+    saveBtn.on("click", function(){
+        var inputText=$(this).siblings("input").val();
+        var timeSlot =$(this).parents().attr("id");
+        localStorage.setItem(timeSlot,inputText);
+    });
+    checkTime();
+    getLocalstorage();
+    });

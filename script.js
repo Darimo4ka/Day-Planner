3d7287afa62ca1ed6afc3e01ac68db5fabb2ff3b
Darimo4ka <darima_badm@yahoo.com>
2022-02-01 18:18:04 -0500

    Figure out colours and now I need to move to local storage again

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 //  ADD CURRENT DATE AT THE TOP OF THE PAGE
-// var today = moment();
-// $("#time-display").text(today.format("dddd, MMMM Do"));
 
 
-// assign var to HTML
-var rowEl = $(".row"); 
-var saveBtn =$(".col-md-1 col-sm-2 saveBtn");
-var todayEl =$("#currentDay");
-var timeDisplay =$("#time-display");
+//  research Document ready function
+$(document).ready(function (){
+    //  ADD CURRENT DATE AT THE TOP OF THE PAGE
+    var today = moment();
+    console.log(today);
 
-function getLocalstorage(){
+
+    $("#time-display").text(today.format("dddd, MMMM Do"));
+    // assign var to HTML
+    var rowEl = $(".row"); 
+    var saveBtn =$(".col-md-1 col-sm-2 saveBtn");
+    var todayEl =$("#currentDay");
+    var timeDisplay =$("#time-display");  
+    //  work with colours and past present future function()
+
+    function checkTime()  {
+
+        var now = moment().hour();
+        rowEl.each(function(){
+        var workTime = parseInt($(this).attr("id").split("row")[1]);
+        
+        if (workTime< now){
+            $(this).children("input").addClass("past");
+
+            // console.log($(this).children("input"))
+            
+        } else if (workTime === now){
+            $(this).children("input").addClass("present");}
+        else{
+            $(this).children("input").addClass("future");
+        }
+    });
+   }
+   function getLocalstorage(){
     rowEl.each(function() {
         var saveTask = $(this).attr("id");
         if(localStorage.getItem(saveTask) !==null){
@@ -18,29 +43,20 @@ function getLocalstorage(){
             
     });
 }
-// handle displaying the time
-function checkTime()  {
+getLocalstorage();
+ checkTime()
+})
 
-    var now = moment().hour();
 
-    rowEl.each(function(){
-        var workTime = parseInt($(this).attr("id").split("row")[1]);
-    if (workTime< now){
-        $(this).children("input").addclass("past");
-    } else if (workTime === now){
-        $(this).children("input").addclass("present");}
-        else{
-            $(this).children("input").addclass("future");
-        }
-    });
-   }
-   $(document).ready(function(){
-    todayEl.text(moment().format("dddd,MMMM Do"));
-    saveBtn.on("click", function(){
-        var inputText=$(this).siblings("input").val();
-        var timeSlot =$(this).parents().attr("id");
-        localStorage.setItem(timeSlot,inputText);
-    });
-    checkTime();
-    getLocalstorage();
-    });
+
+
+//    $(document).ready(function(){
+//     todayEl.text(moment().format("dddd,MMMM Do"));
+//     saveBtn.on("click", function(){
+//         var inputText=$(this).siblings("input").val();
+//         var timeSlot =$(this).parents().attr("id");
+//         localStorage.setItem(timeSlot,inputText);
+//     });
+//     checkTime();
+//     getLocalstorage();
+//     });
